eefacda6965021c3ab21fb71cf9b05371ea244f2
diana <dskripka005@gmail.com>
2024-01-22 20:00:41 +0200

    fixgallery

---
+++ js/main.js
@@ -0,0 +1,40 @@
+import { galleryItems } from "./app.js";
+
+const gallery = document.querySelector(".foto");
+const modalImg = document.querySelector(".image");
+const closeButton = document.querySelector('[data-action="close-lightbox"]');
+const lighboxOverlay = document.querySelector(".lightbox-overlay");
+
+galleryItems.forEach((item) => {
+    gallery.insertAdjacentHTML("beforeend", `<li class="items">
+  <a class="links" href="${item.original}">
+    <img class="gallery-image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+  </a>
+</li>`)
+})
+
+gallery.addEventListener("click", imageChooser);
+closeButton.addEventListener("click", closeModal);
+lighboxOverlay.addEventListener("click", (e) => {
+    if (e.target === lighboxOverlay) {
+        closeModal()
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+        closeModal()
+  }
+})
+
+function imageChooser(e) {
+    if (e.target !== gallery) {
+        let clickedImg = e.target;
+        modalImg.src = clickedImg.src;
+        modalImg.alt = clickedImg.alt;
+        document.body.classList.add("show-modal");
+    }
+}
+
+function closeModal(e) {
+    document.body.classList.remove("show-modal");
+}
